48e51e465d08c561e6698eae1479cd8ab0ae1d33
Alvaro <alvarodcm@gmail.com>
2023-04-27 22:45:18 +0200

    cqambios

--- M05_UF3_P3_Frogger/Program.cs
+++ M05_UF3_P3_Frogger/Program.cs
@@ -2,46 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 
+using M05_UF3_P3_Frogger;
+
 namespace M05_UF3_P3_Frogger
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           // Carreteras
-            Lane[] lanes = new Lane[10];
+            int mapWidth = 40; // Cambia esto al valor deseado, por ejemplo, 40
+            int mapHeight = Utils.MAP_HEIGHT;
+
+            Console.SetWindowSize(mapWidth, mapHeight + 1);
+
+            Lane[] lanes = new Lane[5];
             lanes[0] = new Lane(2, false, ConsoleColor.Blue, true, false, 0.03f, Utils.charCars, Utils.colorsLogs.ToList());
             lanes[1] = new Lane(3, true, ConsoleColor.Green, false, true, 0.1f, Utils.charLogs, Utils.colorsLogs.ToList());
             lanes[2] = new Lane(4, false, ConsoleColor.Blue, true, false, 0.03f, Utils.charCars, Utils.colorsLogs.ToList());
             lanes[3] = new Lane(5, true, ConsoleColor.Green, false, true, 0.1f, Utils.charLogs, Utils.colorsLogs.ToList());
             lanes[4] = new Lane(6, false, ConsoleColor.Blue, true, false, 0.03f, Utils.charCars, Utils.colorsLogs.ToList());
-            lanes[5] = new Lane(7, true, ConsoleColor.Green, false, true, 0.1f, Utils.charLogs, Utils.colorsLogs.ToList());
-            lanes[6] = new Lane(8, false, ConsoleColor.Blue, true, false, 0.03f, Utils.charCars, Utils.colorsLogs.ToList());
-            lanes[7] = new Lane(9, true, ConsoleColor.Green, false, true, 0.1f, Utils.charLogs, Utils.colorsLogs.ToList());
-            lanes[8] = new Lane(10, false, ConsoleColor.Blue, true, false, 0.03f, Utils.charCars, Utils.colorsLogs.ToList());
-            lanes[9] = new Lane(11, true, ConsoleColor.Green, false, true, 0.1f, Utils.charLogs, Utils.colorsLogs.ToList());
 
             Player player = new Player();
 
-            
             Utils.GAME_STATE gameState = Utils.GAME_STATE.RUNNING;
 
-            // Bucle del juego
             while (gameState == Utils.GAME_STATE.RUNNING)
             {
-                // Se recorren las lineas
                 foreach (var lane in lanes)
                 {
                     lane.Update();
                 }
 
-                // Controles
                 Vector2Int tecla = Utils.Input();
-
-                
                 gameState = player.Update(tecla, lanes.ToList());
 
-                // Creamos el mapa
                 Console.Clear();
                 foreach (Lane lane in lanes)
                 {
@@ -50,19 +44,19 @@ namespace M05_UF3_P3_Frogger
                 player.Draw();
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.BackgroundColor = ConsoleColor.Black;
-                Console.SetCursorPosition(0, Utils.MAP_HEIGHT);
+                Console.SetCursorPosition(0, mapHeight);
                 Console.Write("Tiempo: {0:0.00}", TimeManager.time);
 
                 TimeManager.NextFrame();
             }
 
-            // Juego terminado
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.White;
             Console.BackgroundColor = ConsoleColor.Black;
-            Console.WriteLine(gameState == Utils.GAME_STATE.WIN ? "Has ganado!" : "Game over!!!");
-            Console.WriteLine("Pulsa una tecla para salir");
+            Console.WriteLine(gameState == Utils.GAME_STATE.WIN ? "Has ganado!" : "Perdiste...");
+            Console.WriteLine("Pulsa cualquier tecla para salir");
             Console.ReadKey(true);
         }
+
     }
 }
